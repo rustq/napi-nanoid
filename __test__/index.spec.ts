cf3cca1063051d4e57304321840b55d9b062e14b
meloalright <meloalright@gmail.com>
2022-04-30 17:37:10 +0800

    Use the rust nanoid + fast rng and build the benchmark

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -3,5 +3,10 @@ import test from 'ava'
 import { nanoid } from '../index'
 
 test('sync function from native code', (t) => {
-  t.is(nanoid(), 'hello')
+  for (let i = 1; i < 100; i++) {
+    const id = nanoid()
+    // eslint-disable-next-line no-console
+    console.log(id)
+    t.is(id.length, 21)
+  }
 })
